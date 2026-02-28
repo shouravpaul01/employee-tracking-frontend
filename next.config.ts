5bff4e20b9f1cae469e37ee26236996df5e25c44
shouravpaul01 <paul.shourav01@gmail.com>
2026-02-28 21:43:59 +0600

    feature:-- pwa configure

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,12 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development" ? false : true, 
 });
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  turbopack: {}, 
 };
-
 
 export default withPWA(nextConfig as any);
